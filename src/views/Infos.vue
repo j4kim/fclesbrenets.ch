d1584fe22c80594ab7096236af9c4258d8ad33ae
Joaquim Perez <jivkim@gmail.com>
2019-07-12 17:41:35 +0200

    grid layout

--- src/views/Infos.vue
+++ src/views/Infos.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="infos">
     <h1>Infos</h1>
-    <div>
+    <div class="articles">
       <article v-for="article in $parent.data.infos" :key="article.id">
         <h1>{{ article.title }}</h1>
         <div v-html="article.content" />
@@ -15,3 +15,19 @@ export default {
   name: 'infos'
 }
 </script>
+
+<style lang="scss">
+  .articles{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+    article{
+      border:1px solid lightblue;
+      padding:10px;
+      h1{
+        margin:0;
+      }
+    }
+  }
+</style>
+
