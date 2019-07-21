58099e3745afd4d435f869d732a88552a8c6d591
Joaquim Perez <jivkim@gmail.com>
2019-07-21 16:59:04 +0200

    fetch /users to get author name instead of id returned by /posts

--- src/main.js
+++ src/main.js
@@ -17,16 +17,22 @@ Vue.config.productionTip = false
 
 var data = {
     posts: [],
-    pages: []
+    pages: [],
+    users: []
 }
 
-router.beforeEach((to, from, next) => {
-    var source = to.meta.side === 'left' ? 'posts' : 'pages'
-    if(data[source].length === 0){
-        axios.get(process.env.VUE_APP_API + source).then(result => {
-            data[source] = result.data
+function fetchIfNeeded(resource){
+    if(data[resource].length === 0){
+        axios.get(process.env.VUE_APP_API + resource).then(result => {
+            data[resource] = result.data
         })
     }
+}
+
+fetchIfNeeded('users')
+
+router.beforeEach((to, from, next) => {
+    fetchIfNeeded(to.meta.side === 'left' ? 'posts' : 'pages')
     next()
 })
 
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,6 +1,6 @@
 <template>
     <post-or-page :post-or-page="post" back-to="posts">
-        Billet publié le {{ getDate(post.date, 'LL') }} par {{post.author}}
+        Billet publié le {{ getDate(post.date, 'LL') }} par {{ author.name }}
     </post-or-page>
 </template>
 
@@ -14,6 +14,9 @@ export default {
             return this.$root.posts.find(post => {
                 return post.id == this.$route.params.id
             }) || {} // to avoid errors in console when data is not fetched yet
+        },
+        author(){
+            return this.$root.users.find(u => u.id == this.post.author) || {}
         }
     }
 }
