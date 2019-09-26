6d17e17f7ecee5b5e3407fb54eb2ea2cd5f2b6c2
Joaquim Perez <jivkim@gmail.com>
2019-09-26 23:27:17 +0200

    use svg background

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ body,html{
   margin:0;
   padding:0;
   background-color:#cce0f4;
-  background:url('../public/bg.png') no-repeat center center fixed; ;
+  background:url('../public/bg.svg') no-repeat center center fixed; ;
   background-size:cover;
   font-family: 'Nunito', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
