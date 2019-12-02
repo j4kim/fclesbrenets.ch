2d5366210769b968206c08dcf5500aabd12508c3
Joaquim Perez <jivkim@gmail.com>
2019-12-02 10:49:05 +0100

    Request 100 entries in media api request. 100 is the maximum per page :/

--- src/main.js
+++ src/main.js
@@ -16,16 +16,18 @@ Vue.config.productionTip = false
 
 var data = { posts:[], pages:[], users:[], media:[] }
 
-function fetchIfNeeded(resource){
+function fetchIfNeeded(resource, args){
+    // Warning: 10 rows per page by default
+    // todo: pagination for posts and multiple requests for media
     if(data[resource].length === 0){
-        axios.get(process.env.VUE_APP_API + resource).then(result => {
+        axios.get(process.env.VUE_APP_API + resource + "?" + args).then(result => {
             data[resource] = result.data
         })
     }
 }
 
 fetchIfNeeded('users')
-fetchIfNeeded('media')
+fetchIfNeeded('media', "_fields=title,media_details&per_page=100")
 
 router.beforeEach((to, from, next) => {
     fetchIfNeeded(to.meta.side === 'left' ? 'posts' : 'pages')
