c26c93a2d3019df032d2f59e1b34c263af54f67e
Joaquim Perez <jivkim@gmail.com>
2019-12-03 09:06:15 +0100

    wp rest api default params for each resource type

--- src/main.js
+++ src/main.js
@@ -16,9 +16,15 @@ Vue.config.productionTip = false
 
 var data = { posts:[], pages:[], users:[], media:[] }
 
+const DEFAULT_PARAMS = {
+    posts: { _fields: "author,content,date,id,excerpt,slug,sticky,title" },
+    pages: { _fields: "author,content,date,id,excerpt,slug,sticky,title,menu_order" },
+    users: { _fields: "name,id" },
+    media: { _fields: "title,media_details", per_page: 100 }
+}
+
 function fetchResource(resource, params){
-    // Warning: 10 rows per page by default
-    // todo: pagination for posts and multiple requests for media
+    params =  Object.assign({}, DEFAULT_PARAMS[resource], params)
     var baseURL = process.env.VUE_APP_API
     axios.get(resource, {baseURL, params}).then(result => {
         data[resource] = result.data
@@ -28,10 +34,7 @@ function fetchResource(resource, params){
 fetchResource('users')
 fetchResource('posts')
 fetchResource('pages')
-fetchResource('media', {
-    _fields: "title,media_details",
-    per_page: 100
-})
+fetchResource('media')
 
 new Vue({
     router,
