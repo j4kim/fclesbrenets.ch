d884fe45ba801f622f4223095be3ce3687e73eed
Joaquim Perez <jivkim@gmail.com>
2019-12-07 13:31:41 +0100

    fetch functions in root component methods

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import axios from 'axios'
-
 import moment from 'moment'
 moment.locale('fr-ch')
+
 Vue.mixin({
     methods: {
         getDate: (ymd, format = 'L') => moment(ymd).format(format)
     }
 })
-
-Vue.config.productionTip = false
-
-var data = { posts:[], pages:[], users:[], media:[] }
 
 const DEFAULT_PARAMS = {
     posts: { _fields: "author,content,date,id,excerpt,slug,sticky,title" },
@@ -23,30 +18,31 @@ const DEFAULT_PARAMS = {
     media: { _fields: "title,media_details", per_page: 100 }
 }
 
-function fetchPage(resource, page = 1){
-    var params =  Object.assign({ page }, DEFAULT_PARAMS[resource])
-    var baseURL = process.env.VUE_APP_API
-    return axios.get(resource, {baseURL, params}).then(result => {
-        data[resource] = data[resource].concat(result.data)
-        return result
-    })
-}
-
-function fetchAll(resource, pageToFetch = 1){
-    fetchPage(resource, pageToFetch).then(result => {
-        if(result.headers["x-wp-totalpages"] > pageToFetch){
-            fetchAll(resource, pageToFetch + 1)
-        }
-    })
-}
-
-fetchPage('users')
-fetchPage('posts')
-fetchPage('pages')
-fetchAll('media')
-
 new Vue({
     router,
-    data,
-    render: h => h(App)
+    data: { posts:[], pages:[], users:[], media:[] },
+    render: h => h(App),
+    methods:{
+        fetchPage(resource, page = 1){
+            var params =  Object.assign({ page }, DEFAULT_PARAMS[resource])
+            var baseURL = process.env.VUE_APP_API
+            return axios.get(resource, {baseURL, params}).then(result => {
+                this[resource] = this[resource].concat(result.data)
+                return result
+            })
+        },
+        fetchAll(resource, pageToFetch = 1){
+            this.fetchPage(resource, pageToFetch).then(result => {
+                if(result.headers["x-wp-totalpages"] > pageToFetch){
+                    this.fetchAll(resource, pageToFetch + 1)
+                }
+            })
+        }
+    },
+    created(){
+        this.fetchPage('users')
+        this.fetchPage('posts')
+        this.fetchPage('pages')
+        this.fetchAll('media')
+    }
 }).$mount('#app')
