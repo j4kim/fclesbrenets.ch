d3a8968251d41513d1d269c777ff27538559d54e
Joaquim Perez <jivkim@gmail.com>
2020-02-21 00:17:23 +0100

    just 2 cute tooltips

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import VTooltip from 'v-tooltip'
+Vue.use(VTooltip)
 import moment from 'moment'
 moment.locale('fr-ch')
 
