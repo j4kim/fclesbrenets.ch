3626f7aecaada9f0ede984781110396bc69dac1f
Joaquim Perez <jivkim@gmail.com>
2020-09-18 14:01:02 +0200

    use colcade for masonry layout in pages

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,8 +1,12 @@
 <template>
   <section class="pages">
     <h1>Informations générales</h1>
-    <div class="articles">
-      <article v-for="article in orderedPages" :key="article.id">
+    <div class="articles grid">
+      <!-- columns -->
+      <div class="grid-col grid-col--1"></div>
+      <div class="grid-col grid-col--2"></div>
+      <!-- items -->
+      <article class="grid-item" v-for="article in orderedPages" :key="article.id">
         <h1><router-link :to="'infos/' + article.slug">{{ article.title.rendered }}</router-link ></h1>
         <div v-html="article.content.rendered" />
       </article>
@@ -12,6 +16,7 @@
 
 <script>
 import { sortBy } from 'lodash'
+import Colcade from 'colcade'
 
 export default {
   name: 'pages',
@@ -22,14 +27,41 @@ export default {
   },
   mounted() {
     this.$router.options.updateTitle("Infos")
+    new Colcade( '.grid', {
+      columns: '.grid-col',
+      items: '.grid-item'
+    });
   }
 }
 </script>
 
 <style lang="scss">
-  .articles{
-    column-width: 300px;
-    column-gap: 20px;
-  }
+.grid {
+  margin: 0 -10px;
+}
+
+.grid-col {
+  float: left;
+  width: 100%;
+}
+
+.grid:after {
+  display: block;
+  content: '';
+  clear: both;
+}
+
+.grid-item {
+  margin: 0 10px 20px;
+}
+
+/* 1 column, hide columns 2 */
+.grid-col--2 { display: none }
+
+/* 2 columns at medium size */
+@media ( min-width: 800px ) {
+  .grid-col { width: 50%; }
+  .grid-col--2 { display: block; } /* show column 2 */
+}
 </style>
 
