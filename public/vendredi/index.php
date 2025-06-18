d8e875023d989ca2bbd6ee8cd564ea2477a5a64e
Joaquim Perez <jivkim@gmail.com>
2025-06-18 22:18:37 +0200

    adapt locations

--- public/vendredi/index.php
+++ public/vendredi/index.php
@@ -4,4 +4,4 @@ $log = date("Y-m-d H:i:s") . ' | ' . json_encode($_GET);
 
 file_put_contents("visits.log", $log . PHP_EOL, FILE_APPEND | LOCK_EX);
 
-header("Location: https://fclesbrenets.ch/tournoi/?file=Tournoi%20v%C3%A9t%C3%A9rans%2020%20juin%202025%20v1.json");
+header("Location: https://fclesbrenets.ch/tournoi/?file=Tournoi%20des%20Brenets%202025%20-%20vendredi.json");
